3d21d549a61cfc9bb66d331910101ed4756ac2e8
YvanDroid <yvandroid@outlook.com>
2022-09-28 21:22:21 +0100

    Initial commit

---
+++ window_logger.py
@@ -0,0 +1,19 @@
+import win32gui, time, win32process
+
+
+def get_foreground():
+    return win32gui.GetForegroundWindow()
+
+last_window = 0
+while True:
+    current_window = get_foreground()
+    if current_window != last_window:
+        process_id = win32process.GetWindowThreadProcessId(current_window)
+        window_title = win32gui.GetWindowText(current_window)
+        print(f'{current_window} = {window_title}')
+        print(f'Process ID: {process_id}\n' )
+        last_window = current_window
+    if current_window == last_window:
+        pass
+    
+    time.sleep(0.001)
